b76bc57b8b0d94bb142711464188fc40abeb3145
cliren <surenkatik@gmail.com>
2017-06-12 09:41:09 -0700

    RNIntegration initial

---
+++ ios/Pods/Target Support Files/Pods-RNIntegration/Pods-RNIntegration-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNIntegration : NSObject
+@end
+@implementation PodsDummy_Pods_RNIntegration
+@end
---
+++ ios/RNIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RNIntegration
+//
+//  Created by Katikaneni, Surender on 6/12/17.
+//  Copyright © 2017 Goodreads, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
